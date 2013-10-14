93c356485c9e724ca9a984b0748a215c6f8d9465
onjiro <onjiro.mohyahya@gmail.com>
2013-10-14 06:37:38 +0000

    Use annotation based routing

--- src/Onjiro/Bundle/KakeiboBundle/Controller/DefaultController.php
+++ src/Onjiro/Bundle/KakeiboBundle/Controller/DefaultController.php
@@ -2,10 +2,16 @@
 
 namespace Onjiro\Bundle\KakeiboBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/kakeibo/{name}")
+     * @Template()
+     */
     public function indexAction($name)
     {
         return $this->render('OnjiroKakeiboBundle:Default:index.html.twig', array('name' => $name));
